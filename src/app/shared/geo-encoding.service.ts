511d54d5c9149d31d7391c5497f7e9f85982ee72
Hamilton Chevez <hchev001@fiu.edu>
2018-01-14 01:11:31 -0500

    added ZipCodeLocation for returning cardinal coordinate payload

--- src/app/shared/geo-encoding.service.ts
+++ src/app/shared/geo-encoding.service.ts
@@ -1,8 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GeoEncodingService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
+  /**
+   * Returns the Google API key for the GeoEncoding Service
+   */
+  private GeoEncodingKey(): string {
+    return 'AIzaSyCQ-qEuIExQyWTIoSrl39B964yYne08-ZU';
+  }
+
+  /**
+   * Uses Google's GeoEncoding service to retrieve cardinal coordinates based 
+   * on ZipCode
+   * @param zip Zipcode used for retrieving geolocation coordinates
+   */
+  public ZipCodeLocation(zip: string) {
+    const baseUrl = this.GeoBaseUrl();
+    const key = this.GeoEncodingKey();
+    const endpoint = baseUrl + 'address=' + zip + '&key=' + this.GeoBaseUrl();
+    return this.http.get(endpoint);
+  }
+
+  /**
+   * Returns the base url for Google's GeoEncoding service
+   */
+  public GeoBaseUrl(): string {
+    return 'http://maps.googleapis.com/maps/api/geocode/json?';
+  }
 }
